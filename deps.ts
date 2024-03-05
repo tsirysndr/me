d2870ae5e3f51d180ee178f6b35b8100fece7b27
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-03-05 05:41:36 +0000

    chore: use latest version of deno std (v0.218.2)

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { serve } from "https://deno.land/std@0.190.0/http/server.ts";
+export { serve } from "https://deno.land/std@0.218.2/http/server.ts";
 import html, { h } from "https://deno.land/x/htm@0.2.1/mod.ts";
 export { html, h };
 import dayjs from "npm:dayjs";
