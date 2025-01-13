006007e6e8daf3dcabdbcc9b052fc2d965286912
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-01-13 10:07:59 +0000

    add bluesky link

--- src/social/mod.tsx
+++ src/social/mod.tsx
@@ -1,6 +1,7 @@
 /** @jsx h  */
 import { h } from "../../deps.ts";
 import { twitter } from "../twitter.tsx";
+import { bluesky } from "./bluesky.tsx";
 import { github } from "./github.tsx";
 import { linkedin } from "./linkedin.tsx";
 
@@ -8,6 +9,7 @@ export function social() {
   return (
     <div style="display: flex; flex-direction: row; justify-content: space-around; width: 100px;">
       {github()}
+      {bluesky()}
       {twitter()}
       {linkedin()}
     </div>
