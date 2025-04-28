e1aa23d19531103f34b4649ec613afa2849292a7
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-04-28 20:40:35 +0300

    update _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,11 @@
 import { type PageProps } from "$fresh/server.ts";
+
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
+}
+
 export default function App({ Component }: PageProps) {
   return (
     <html>
@@ -8,6 +15,21 @@ export default function App({ Component }: PageProps) {
         <title>Tsiry Sandratraina</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
+
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-2LP61SRX58"
+      >
+      </script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag(){window
+          .dataLayer.push(arguments)}
+
+        gtag('js', new Date());
+
+        gtag('config', 'G-2LP61SRX58');
+      </script>
+
       <body>
         <Component />
       </body>
