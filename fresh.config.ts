db925a2441c445697e5e467c2e7ed19084561487
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-04-28 21:06:48 +0300

    configure fresh ga plugin

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,7 @@
 import tailwind from "$fresh/plugins/tailwind.ts";
 import { defineConfig } from "$fresh/server.ts";
+import { gaPlugin } from "ga/mod.ts";
 
 export default defineConfig({
-  plugins: [tailwind()],
+  plugins: [tailwind(), gaPlugin({ enableServerGa: true })],
 });
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -9,11 +9,6 @@ export default function App({ Component }: PageProps) {
         <title>Tsiry Sandratraina</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-2LP61SRX58"
-      >
-      </script>
       <body>
         <Component />
       </body>
